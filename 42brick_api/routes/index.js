272f9dc7358d18555bbed8733374de6fd7af8f0d
octo <dlqhdgkr456@gmail.com>
2022-07-19 13:56:45 +0900

    test api

--- 42brick_api/routes/index.js
+++ 42brick_api/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({
+    "message": "main success!!",
+  });
 });
 
 module.exports = router;
