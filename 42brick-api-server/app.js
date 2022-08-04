4824dc5edc40cbe613b9c5551349721ea69342e3
LEEBONGHAK <dlqhdgkr456@gmail.com>
2022-08-04 17:22:52 +0900

    created test-api.js

--- 42brick-api-server/app.js
+++ 42brick-api-server/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/test');
+var testAPIRouter = require('./routes/test-api');
 
 var app = express();
 
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
+app.use('/test-api', testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
