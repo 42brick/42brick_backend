4251a75e37d8eadaec8bda040607bddded09c354
octo <dlqhdgkr456@gmail.com>
2022-08-05 17:01:55 +0900

    modified nft-data.js and app.js

--- 42brick-api-server/app.js
+++ 42brick-api-server/app.js
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/get-nfts', getNFTsRouter);
 app.use('/search-nfts', searchNFTsRouter);
-app.use('/ntf-data', nftDataRouter);
+app.use('/nft-data', nftDataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- 42brick-api-server/routes/nft-data.js
+++ 42brick-api-server/routes/nft-data.js
@@ -17,23 +17,64 @@ const router = express.Router();
  *         required: true
  *         schema:
  *           type: string
+ *         examples:
+ *           Sample:
+ *            value: "0xb5643598496b159263c67bd0d25728713f5aad04"
+ *            summary: example contract address
  *       - name: id
  *         in: query
  *         required: true
  *         schema:
  *           type: string
+ *         examples:
+ *           Sample:
+ *            value: "4096"
+ *            summary: example token id
  *       - name: symbol
  *         in: query
  *         required: true
  *         schema:
  *           type: string
+ *         examples:
+ *           Sample:
+ *            value: "eth"
+ *            summary: example chain symbol
  *     responses:
  *       200:
- *         description: Returns NFTs searched by keyword.
+ *         description: Returns NFT's datas
  *         content:
  *           application/json:
  *             schema:
  *               type: object
+ *               properties:
+ *                 token_address:
+ *                   type: string
+ *                 token_id:
+ *                   type: string
+ *                 owner_of:
+ *                   type: string
+ *                 block_number:
+ *                   type: string
+ *                 block_number_minted:
+ *                   type: string
+ *                 token_hash:
+ *                   type: string
+ *                 amount:
+ *                   type: string
+ *                 contract_type:
+ *                   type: string
+ *                 name:
+ *                   type: string
+ *                 symbol:
+ *                   type: string
+ *                 token_uri:
+ *                   type: string
+ *                 metadata:
+ *                   type: string
+ *                 last_token_uri_sync:
+ *                   type: string
+ *                 last_metadata_sync:
+ *                   type: string
  */
 router.get('/', async (req, res, next) => {
     
@@ -41,6 +82,10 @@ router.get('/', async (req, res, next) => {
     const addr = req.query.addr;
     const id = req.query.id;
     const symbol = req.query.symbol;
+
+    console.log(addr)
+    console.log(id)
+    console.log(symbol)
 
     const options = {
         address: addr,
