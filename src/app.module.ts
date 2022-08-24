8435aebfcbb0cb6c890828865dbad5d6a3b23e71
octo <dlqhdgkr456@gmail.com>
2022-08-24 19:57:20 +0900

    remove UtilsService in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NftModule } from './nft/nft.module';
-import { UtilsService } from './utils/utils.service';
 
 @Module({
   imports: [NftModule],
   controllers: [AppController],
-  providers: [AppService, UtilsService],
+  providers: [AppService],
 })
 export class AppModule {}
