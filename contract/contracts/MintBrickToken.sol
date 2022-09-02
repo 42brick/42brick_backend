83b83a463386dad62e96d28a819e40b7119d6cc5
octo <dlqhdgkr456@gmail.com>
2022-09-02 18:21:54 +0900

    prepare for making contract

---
+++ contract/contracts/MintBrickToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Enumerable.sol";
+
+contract MintBrickToken is ERC721Enumerable {
+	constructor() {}
+}
